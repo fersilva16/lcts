6414b48ade500d2796f0e2d6a2bdf68f01fa99b0
Fernando Silva <fernandonsilva16@gmail.com>
2022-05-28 10:35:41 -0300

    feat(pretty-print): improve pretty printing

--- src/PrettyPrint.ts
+++ src/PrettyPrint.ts
@@ -1,11 +1,17 @@
 import type { Dot, Lambda, LeftPar, RightPar, Space } from './Char';
-import type { Abs, App, Expr, Var } from './Expr';
+import type { Abs, App, Arg, Expr, Func, Var } from './Expr';
 
 export type PrettyPrint<E extends Expr> = E extends Var<infer N>
   ? N
   : E extends Abs<infer P, infer B>
   ? // @ts-expect-error
-    `${Lambda}${P}${Dot}${LeftPar}${PrettyPrint<B>}${RightPar}`
+    `${Lambda}${P}${Dot}${PrettyPrint<B>}`
+  : E extends App<App<Expr, Expr>, App<Expr, Expr>>
+  ? `${LeftPar}${PrettyPrint<
+      Func<E>
+    >}${RightPar}${Space}${LeftPar}${PrettyPrint<Arg<E>>}${RightPar}`
+  : E extends App<Abs<string, Expr> | App<Expr, Expr>, infer A>
+  ? `${LeftPar}${PrettyPrint<Func<E>>}${RightPar}${Space}${PrettyPrint<A>}`
   : E extends App<infer F, infer A>
-  ? `${LeftPar}${PrettyPrint<F>}${RightPar}${Space}${LeftPar}${PrettyPrint<A>}${RightPar}`
+  ? `${PrettyPrint<F>}${Space}${PrettyPrint<A>}`
   : never;
