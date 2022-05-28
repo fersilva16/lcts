4e85bd095583a4b64052520a1e7b8d1ddc637e4c
Fernando Silva <fernandonsilva16@gmail.com>
2022-05-28 10:52:56 -0300

    test(lambda-calculus): fix tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -3,7 +3,7 @@ import type { LambdaCalculus } from '..';
 
 type cases = [
   Expect<Equal<LambdaCalculus<'x'>, 'x'>>,
-  Expect<Equal<LambdaCalculus<'(λy.y)'>, 'λy.(y)'>>,
+  Expect<Equal<LambdaCalculus<'(λy.y)'>, 'λy.y'>>,
   Expect<Equal<LambdaCalculus<'(λy.y) x'>, 'x'>>,
-  Expect<Equal<LambdaCalculus<'(λy.y) (λy.y)'>, 'λy.(y)'>>
+  Expect<Equal<LambdaCalculus<'(λy.y) (λy.y)'>, 'λy.y'>>
 ];
