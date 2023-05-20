7b02af0d63348982cb976ec394a5defc92b2f444
Fernando Silva <fernandonsilva16@gmail.com>
2023-05-20 19:56:11 -0300

    feat(pretty-print): simplify

--- src/PrettyPrint.ts
+++ src/PrettyPrint.ts
@@ -1,24 +1,22 @@
-import type { CDot, CLambda, CLeftPar, CRightPar, CSpace } from './data/Char';
 import type { EAbs, EApp, EArg, EBody, Expr, EFunc, EVar } from './data/Expr';
 
 export type PrettyPrint<E extends Expr> = E extends EVar<infer N>
   ? N
   : E extends EAbs<infer P, EApp<EVar<string>, EVar<string>>>
-  ? `${CLambda}${P}${CDot}${PrettyPrint<EBody<E>>}`
+  // @ts-ignore
+  ? `λ${P}.${PrettyPrint<EBody<E>>}`
   : E extends EAbs<infer P, EApp<Expr, Expr>>
-  ? `${CLambda}${P}${CDot}${CLeftPar}${PrettyPrint<EBody<E>>}${CRightPar}`
+  ? `λ${P}.(${PrettyPrint<EBody<E>>})`
   : E extends EAbs<infer P, infer B>
-  ? `${CLambda}${P}${CDot}${PrettyPrint<B>}`
+  ? `λ${P}.${PrettyPrint<B>}`
   : E extends EApp<EApp<Expr, Expr>, EApp<Expr, Expr>>
-  ? `${CLeftPar}${PrettyPrint<
-      EFunc<E>
-    >}${CRightPar}${CSpace}${CLeftPar}${PrettyPrint<EArg<E>>}${CRightPar}`
+  ? `(${PrettyPrint<EFunc<E>>}) (${PrettyPrint<EArg<E>>})`
   : E extends EApp<EVar<string>, EApp<Expr, Expr>>
-  ? `${PrettyPrint<EFunc<E>>}${CSpace}${CLeftPar}${PrettyPrint<
+  ? `${PrettyPrint<EFunc<E>>} (${PrettyPrint<
       EArg<E>
-    >}${CRightPar}`
+    >})`
   : E extends EApp<EAbs<string, Expr> | EApp<Expr, Expr>, infer A>
-  ? `${CLeftPar}${PrettyPrint<EFunc<E>>}${CRightPar}${CSpace}${PrettyPrint<A>}`
+  ? `(${PrettyPrint<EFunc<E>>}) ${PrettyPrint<A>}`
   : E extends EApp<infer F, infer A>
-  ? `${PrettyPrint<F>}${CSpace}${PrettyPrint<A>}`
+  ? `${PrettyPrint<F>} ${PrettyPrint<A>}`
   : never;
