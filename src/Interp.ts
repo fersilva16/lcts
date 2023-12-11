fc2221014d812e5eac3710b2a45d92fc70d941b6
Fernando Silva <fernandonsilva16@gmail.com>
2023-12-11 20:29:04 -0300

    fix(interp): fix

--- src/Interp.ts
+++ src/Interp.ts
@@ -1,5 +1,4 @@
 import type { EAbs, EApp, Expr, EVar } from './data/Expr';
-import { Assert } from './utils/Assert';
 import { Overwrite } from './utils/Overwrite';
 
 type Substitute<
@@ -8,18 +7,23 @@ type Substitute<
   C extends Record<string, Expr>
 > = E extends EVar<N>
   ? C[N]
-  : E extends EAbs<infer X, infer Y>
-  ? EAbs<X, Substitute<N, Y, Overwrite<C, X, EVar<X>>>>
-  : E extends EApp<infer X, infer Y>
-  ? EApp<Substitute<N, X, C>, Substitute<N, Y, C>>
+  : E extends EAbs<infer P, infer B>
+  ? EAbs<P, Substitute<N, B, Overwrite<C, P, EVar<P>>>>
+  : E extends EApp<infer F, infer A>
+  ? EApp<Substitute<N, F, C>, Substitute<N, A, C>>
   : E;
 
-export type Interp<E extends Expr, C extends Record<string, Expr> = {}> = E extends EVar<infer X>
-  ? EVar<X>
-  : E extends EAbs<infer X, infer Y>
-  ? EAbs<X, Interp<Y>>
-  : E extends EApp<EAbs<infer X, infer Y>, infer Z>
-  ? Interp<Substitute<X, Y, Overwrite<C, X, Z>>>
-  : E extends EApp<infer X, infer Y>
-  ? EApp<Interp<X>, Interp<Y>>
+export type Interp<
+  E extends Expr,
+  C extends Record<string, Expr> = {}
+> = E extends EVar<string>
+  ? E
+  : E extends EAbs<infer P, infer B>
+  ? EAbs<P, Interp<B, C>>
+  : E extends EApp<EAbs<infer P, infer B>, infer A>
+  ? Interp<Substitute<P, B, Overwrite<C, P, A>>, Overwrite<C, P, A>>
+  : E extends EApp<EVar<infer N>, infer A>
+  ? EApp<EVar<N>, Interp<A, C>>
+  : E extends EApp<infer F, infer A>
+  ? Interp<EApp<Interp<F, C>, Interp<A, C>>, C>
   : never;
